c9e759016b3d8d927213ad281388c649a172fb5b
lenovo <lenovo@LAPTOP-9LFOOVPF>
2020-05-21 16:56:44 +0800

    搭建三层及Model类

---
+++ HouseModel/PrincipalModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseModel
+{
+    /// <summary>
+    /// 经纪人表
+    /// </summary>
+    public class PrincipalModel
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string PrincipalName { get; set; }
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public long PrincipalPhone { get; set; }
+        /// <summary>
+        /// 所属公司
+        /// </summary>
+        public int CommodityId { get; set; }
+        /// <summary>
+        /// 工作经验
+        /// </summary>
+        public int Enter { get; set; }
+        /// <summary>
+        /// 个人照
+        /// </summary>
+        public string PrImage { get; set; }
+        
+    }
+}
