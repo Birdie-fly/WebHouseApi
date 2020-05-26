3d33d225ae21f7d91053544b40f720273577a93a
刮猪毛 <1487858371@qq.com>
2020-05-26 22:12:22 +0800

    登陆

---
+++ HouseBLL/LandingBll.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseBLL
+{
+    /// <summary>
+    /// 登陆
+    /// </summary>
+    public class LandingBll
+    {
+    }
+}
---
+++ HouseDAL/LandingDal.cs
@@ -0,0 +1,28 @@
+﻿using HouseModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseDAL
+{
+    /// <summary>
+    /// 登陆
+    /// </summary>
+    public class LandingDal
+    {
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+       // private static readonly string connectionString = @"Data Source=47.92.51.7;Initial Catalog=House;Persist Security Info=True;User ID=sa;Pwd=qwer123456@;";
+
+        //登陆
+         public int  LandingModel(string Name,string Pwd)
+        {
+            string sql = $"select count(1) from LandingModel where Name='{Name}' and Pwd='{Pwd}'";
+            return Convert.ToInt32(DapperHelper<LandingModel>.ExecuteScalar(sql,null));
+        }
+
+
+    }
+}
