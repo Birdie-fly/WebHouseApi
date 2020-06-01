949078ed2e0dcb95e9159495ac554e8419bab242
刮猪毛 <1487858371@qq.com>
2020-06-01 20:00:40 +0800

    登陆

--- WebHouseApi/Startup.cs
+++ WebHouseApi/Startup.cs
@@ -33,6 +33,9 @@ namespace WebHouseApi
 
             services.AddControllers();
 
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ WebHouseMVC/WebHouseMVC/Controllers/LandingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebHouseMVC.Controllers
+{
+    public class LandingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //登陆
+
+        public IActionResult Landing(string Name, string Pwd)
+        {
+            return View(Name,Pwd);
+        }
+
+        //注册
+        public IActionResult Reister()
+        {
+            return View();
+        }
+
+
+    }
+}
