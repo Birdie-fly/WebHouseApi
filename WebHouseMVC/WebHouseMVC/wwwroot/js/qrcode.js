342e1f0afd860911daf4ac422b914da9cf7f291d
王勇彪 <2499019486@qq.com>
2020-06-10 18:42:23 +0800

    11

--- WebHouseMVC/WebHouseMVC/wwwroot/js/qrcode.js
+++ WebHouseMVC/WebHouseMVC/wwwroot/js/qrcode.js
@@ -526,16 +526,16 @@ var QRCode;
 	 * @param {HTMLElement|String} el target element or 'id' attribute of element.
 	 * @param {Object|String} vOption
 	 * @param {String} vOption.text QRCode link data
-	 * @param {Number} [vOption.width=256]
-	 * @param {Number} [vOption.height=256]
+	 * @param {Number} [vOption.width=150]
+	 * @param {Number} [vOption.height=150]
 	 * @param {String} [vOption.colorDark="#000000"]
 	 * @param {String} [vOption.colorLight="#ffffff"]
 	 * @param {QRCode.CorrectLevel} [vOption.correctLevel=QRCode.CorrectLevel.H] [L|M|Q|H] 
 	 */
 	QRCode = function (el, vOption) {
 		this._htOption = {
-			width : 100, 
-			height : 100,
+			width : 150, 
+			height : 150,
 			typeNumber : 4,
 			colorDark : "#000000",
 			colorLight : "#ffffff",
